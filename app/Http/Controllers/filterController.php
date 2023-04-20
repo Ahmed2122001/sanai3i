2bbd3762ebbbff9ca9044c1a02ac144b0c299cd6
Zwedy <60856824+abdalla58@users.noreply.github.com>
2023-04-20 16:43:09 +0200

    filteration

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rate;
 use Illuminate\Http\Request;
 use App\Models\category;
 use App\Models\region;
@@ -26,20 +27,54 @@ class filterController extends Controller
         return response()->json($worker);
     }
     //filter by category and region
-    public function filterByCategoryAndRegion($category_id, $city_id)
+    public function filterByCategoryAndRegion(Request $request)
     {
+        $category_id = $request->category_id;
+        $city_id = $request->city_id;
         $worker = worker::select('name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
             ->where('category_id', $category_id)
             ->where('city_id', $city_id)
             ->get();
         return response()->json($worker);
     }
-    //filter by rate
-    public function filterByRate($rate)
+
+    //filter by price rate and quality rate and time rate which all integers
+    public function filterByPriceRateAndQualityRateAndTimeRate(Request $request)
     {
+        $price_rate = $request->price_rate;
+        $quality_rate = $request->quality_rate;
+        $time_rate = $request->time_rate;
         $worker = worker::select('name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
-            ->where('rate', $rate)
+            ->join('rate', 'worker.id', '=', 'rate.worker_id')
+            ->where('price_rate', $price_rate)
+            ->where('quality_rate', $quality_rate)
+            ->where('time_rate', $time_rate)
             ->get();
         return response()->json($worker);
+    }
+
+    //filter by data come from user which may contain category_id and city_id and price_rate and quality_rate and time_rate
+    public function filter(Request $request){
+        $query = worker::select('name', 'phone', 'address', 'city_id', 'image', 'description', 'status');
+        if($request->has('category_id')){
+            $query->where('category_id',$request->category_id);
+        }
+        if($request->has('city_id')){
+            $query->where('city_id',$request->city_id);
+        }
+        if($request->has('price_rate') || $request->has('quality_rate') || $request->has('time_rate')){
+            $query->leftJoin('rate', 'worker.id', '=', 'rate.worker_id');
+        }
+        if($request->has('price_rate')){
+            $query->Where('price_rate',$request->price_rate);
+        }
+        if($request->has('quality_rate')){
+            $query->Where('quality_rate',$request->quality_rate);
+        }
+        if($request->has('time_rate')){
+            $query->Where('time_rate',$request->time_rate);
+        }
+        $workers = $query->get();
+        return response()->json($workers);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,10 @@ use App\Http\Controllers\filterController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('/categories', [categoryController::class, 'index']);
-Route::get('/categories/{id}', [categoryController::class, 'show']);
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 
 
@@ -37,10 +35,7 @@ Route::group(
     function ($router) {
         Route::post('/login', [AuthController::class, 'loginAsCustomer']);
         Route::post('/customer/register', [AuthController::class, 'customerRegister']);
-        Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.gaurd:api-customer');
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::get('/user-profile', [AuthController::class, 'userProfile']);
-        Route::get('/all-region', [RegionController::class, 'showAllRegions'])->middleware('auth.gaurd:api-customer');
+
     }
 );
 Route::group(
@@ -51,10 +46,7 @@ Route::group(
     function ($router) {
         Route::post('/login', [AuthController::class, 'loginAsWorker']);
         Route::post('/worker/register', [AuthController::class, 'workerRegister']);
-        Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.gaurd:api-worker');
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::get('/user-profile', [AuthController::class, 'userProfile']);
-        Route::get('/all-region', [RegionController::class, 'showAllRegions'])->middleware('auth.gaurd:api-worker');
+
     }
 );
 Route::group(
@@ -66,66 +58,6 @@ Route::group(
         Route::post('/login', [AuthController::class, 'loginAsAdmin']);
     }
 );
-Route::group(
-    [
-        //        'middleware' => 'api',
-        //        'middleware' => 'auth.gaurd:api-admin',
-        'prefix' => 'sanai3i'
-    ],
-    function ($router) {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::get('/user-profile', [AuthController::class, 'userProfile']);
-        Route::post('/categories', [categoryController::class, 'create']);
-        Route::put('/categories/{id}', [categoryController::class, 'update']);
-        Route::delete('/categories/{id}', [categoryController::class, 'delete']);
-        Route::prefix('Filter')->group(function () {
-            Route::get('/category/{id}', [filterController::class, 'filterByCategory']);
-        });
-        //admin
-        // Route::prefix('/admin')->group(function () {
-        //     //admin
-        //     Route::get('/admins', [AdminController::class, 'index']);
-        //     Route::post('/addadmins', [AdminController::class, 'addAdmin']);
-        //     Route::put('/{admin}', [AdminController::class, 'update']);
-        //     Route::delete('/{admin}', [AdminController::class, 'delete']);
-        //     //functionality
-        //     Route::get('/fix/{id}', [AdminController::class, 'fixAccount']);
-        //     Route::delete('/allusers/deletecustomer/{id}', [AdminController::class, 'deleteCustomer']);
-        //     Route::put('/allusers/suspendcustomer/{id}', [AdminController::class, 'suspendCustomer']);
-        //     Route::put('/allusers/activate/{id}', [AdminController::class, 'activateCustomer']);
-        //     Route::delete('/allusers/deleteworker/{id}', [AdminController::class, 'deleteWorker']);
-        //     Route::put('/allusers/suspendworker/{id}', [AdminController::class, 'suspendWorker']);
-        //     Route::put('/allusers/verifyworker/{id}', [AdminController::class, 'verifyWorker']);
-        //     Route::get('/allusers', [AdminController::class, 'showAllAccounts']);
-        //     Route::post('/category', [AdminController::class, 'createCategory']);
-        //     Route::get('/reports', [AdminController::class, 'showReports']);  //show all reports
-
-    }
-);
-
-
-
-Route::prefix('customer')->group(function () {
-
-    //customer
-    Route::get('/customers', [CustomerController::class, 'index']);
-    Route::POST('/store', [CustomerController::class, 'store']);
-    Route::GET('/show/{id}', [CustomerController::class, 'show']);
-    Route::PUT('/update/{id}', [CustomerController::class, 'update']);
-    Route::DELETE('/delete/{id}', [CustomerController::class, 'delete']);
-});
-
-
-Route::prefix('worker')->group(function () {
-    //workers
-    Route::get('/workers', [WorkerController::class, 'index']);
-    Route::post('/store', [WorkerController::class, 'store']);
-    Route::get('/show/{id}', [WorkerController::class, 'show']);
-    Route::post('/update/{id}', [WorkerController::class, 'update']);
-    Route::delete('/delete/{id}', [WorkerController::class, 'delete']);
-    Route::post('/password/{id}', [WorkerController::class, 'updatePassword']);
-});
 
 Route::group(
     [
@@ -140,6 +72,8 @@ Route::group(
             Route::post('/addadmins', [AdminController::class, 'addAdmin']);
             Route::put('/{admin}', [AdminController::class, 'update']);
             Route::delete('/{admin}', [AdminController::class, 'delete']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+
             //functionality
             Route::get('/fix/{id}', [AdminController::class, 'fixAccount']);
             Route::delete('/allusers/deletecustomer/{id}', [AdminController::class, 'deleteCustomer']);
@@ -165,6 +99,50 @@ Route::group(
         });
     }
 );
-Route::post('/sanai3i/login', [AuthController::class, 'loginAsAdmin']);
-//Route::post('/loginWorker', [AuthController::class, 'loginAsWorker']);
-//Route::post('/loginCustomer', [AuthController::class, 'loginAsCustomer']);
+
+
+//create route group for customer
+Route::group(
+    [
+        'middleware' => 'auth.gaurd:api-customer',
+        'prefix' => 'sanai3i'
+    ],
+    function ($router) {
+        Route::prefix("customer")->group(function (){
+            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::post('/refresh', [AuthController::class, 'refresh']);
+            Route::get('/user-profile', [AuthController::class, 'userProfile']);
+            Route::get('/all-region', [RegionController::class, 'showAllRegions']);
+            //filters
+            Route::get('/category/{id}', [FilterController::class, 'filterByCategory']);
+            Route::get('/region/{id}', [FilterController::class, 'filterByRegion']);
+            Route::get('/category/{id}/region/{id}', [FilterController::class, 'filterByCategoryAndRegion']);
+            // filter by category
+            Route::get('/category/{id}', [filterController::class, 'filterByCategory']);
+            // filter by region
+            Route::get('/region/{id}', [filterController::class, 'filterByRegion']);
+            // filter by category and region
+            Route::post('/filterByCategoryAndRegion', [filterController::class, 'filterByCategoryAndRegion']);
+            //  filterByPriceRateAndQualityRateAndTimeRate
+            Route::post('/filterByPriceRateAndQualityRateAndTimeRate', [filterController::class, 'filterByPriceRateAndQualityRateAndTimeRate']);
+            // filter by category and region and price rate and quality rate and time rate
+            Route::post('filtration',[filterController::class,'filter']);
+        });
+    }
+);
+//create route group for worker
+Route::group(
+    [
+        'middleware' => 'auth.gaurd:api-worker',
+        'prefix' => 'worker'
+    ],
+    function ($router) {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::get('/user-profile', [AuthController::class, 'userProfile']);
+        Route::get('/all-region', [RegionController::class, 'showAllRegions']);
+        Route::post('/password/{id}', [WorkerController::class, 'updatePassword']);
+        Route::post('/update/{id}', [WorkerController::class, 'update']);
+
+    }
+);
