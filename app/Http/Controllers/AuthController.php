f431f7a13698219f2367378f542b615d34401754
abdalla said <abdallasaid201@gmail.com>
2023-04-26 16:13:09 +0200

    worker request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -96,7 +96,7 @@ class AuthController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json($validator->errors(), 400);
         }
         $user = Worker::create(array_merge(
             $validator->validated(),
