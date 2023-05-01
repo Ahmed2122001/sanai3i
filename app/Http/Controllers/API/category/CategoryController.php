e73c35c5cbbb4e7615fe16648e30d5b0494dace3
abdalla said <abdallasaid201@gmail.com>
2023-05-01 18:12:01 +0300

    finaly category name changed to CategoryController

--- app/Http/Controllers/API/category/CategoryController.php
+++ app/Http/Controllers/API/category/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
-class categoryController extends Controller
+class CategoryController extends Controller
 {
     public function index()
     {
