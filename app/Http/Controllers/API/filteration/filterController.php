8df7de19622dc3dcf3e8b8ac94cccbc305062565
abdalla said <abdallasaid201@gmail.com>
2023-06-30 14:41:44 +0300

    add try and cach for filtiraton function

--- app/Http/Controllers/API/filteration/filterController.php
+++ app/Http/Controllers/API/filteration/filterController.php
@@ -53,44 +53,52 @@ class filterController extends Controller
 
     //filter by data come from user which may contain category_id and city_id and price_rate and quality_rate and time_rate
     public function filter(Request $request){
-        $query = worker::select( 'id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status');
-        // check image
+        try{
+            $query = worker::select('id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status');
+            // check image
 
-        if($request->has('category_id')){
-            $query->where('category_id',$request->category_id);
-        }
-        if($request->has('city_id')){
-            $query->where('city_id',$request->city_id);
-        }
-        if($request->has('price_rate') || $request->has('quality_rate') || $request->has('time_rate')){
-            $query->leftJoin('rate', 'worker.id', '=', 'rate.worker_id');
-        }
-        if($request->has('price_rate')){
-            $query->Where('price_rate',$request->price_rate);
-        }
-        if($request->has('quality_rate')){
-            $query->Where('quality_rate',$request->quality_rate);
-        }
-        if($request->has('time_rate')){
-            $query->Where('time_rate',$request->time_rate);
-        }
-        if ($query->get()->isEmpty()) {
-            return response()->json(['message' => 'no data found'], 404);
-        }
-        $workers = $query->get();
+            if ($request->has('category_id')) {
+                $query->where('category_id', $request->category_id);
+            }
+            if ($request->has('city_id')) {
+                $query->where('city_id', $request->city_id);
+            }
+            if ($request->has('price_rate') || $request->has('quality_rate') || $request->has('time_rate')) {
+                $query->leftJoin('rate', 'worker.id', '=', 'rate.worker_id');
+            }
+            if ($request->has('price_rate')) {
+                $query->Where('price_rate', $request->price_rate);
+            }
+            if ($request->has('quality_rate')) {
+                $query->Where('quality_rate', $request->quality_rate);
+            }
+            if ($request->has('time_rate')) {
+                $query->Where('time_rate', $request->time_rate);
+            }
+            if ($query->get()->isEmpty()) {
+                return response()->json(['message' => 'no data found'], 404);
+            }
+            $workers = $query->get();
 
-        foreach ($workers as $worker) {
-            if ($worker->image != null) {
+            foreach ($workers as $worker) {
+                if ($worker->image != null) {
 
-                $path = public_path($worker->image);
-                if (file_exists($path)) {
-                    $file = file_get_contents($path);
-                    $base64 = base64_encode($file);
-                    $worker->image = $base64;
+                    $path = public_path($worker->image);
+                    if (file_exists($path)) {
+                        $file = file_get_contents($path);
+                        $base64 = base64_encode($file);
+                        $worker->image = $base64;
+                    }
                 }
             }
+            return response()->json($workers);
+        }catch (\Throwable $th){
+            return response()->json([
+                'success' => false,
+                'message'=>'حدث خطأ ما',
+                'errors' => $th->getMessage(),
+            ], 404);
         }
-        return response()->json($workers);
     }
 }
 
