e6c798848dde386c884dd281192eba57f933046f
Zwedy <60856824+abdalla58@users.noreply.github.com>
2023-07-03 21:05:54 +0300

    Update filterController.php

--- app/Http/Controllers/API/filteration/filterController.php
+++ app/Http/Controllers/API/filteration/filterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API\filteration;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
-use App\Models\worker;
+use App\Models\Worker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
@@ -13,50 +13,128 @@ class filterController extends Controller
     //filter by category
     public function filterByCategory($id)
     {
-        $worker = worker::select('worker.id','name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
-            ->where('category_id', $id)
-            ->get();
-        return response()->json($worker);
+        try{
+            $workers = Worker::select('worker.id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
+                ->where('category_id', $id)
+                ->get();
+            foreach ($workers as $worker) {
+                if ($worker->image != null) {
+
+                    $path = public_path($worker->image);
+                    if (file_exists($path)) {
+                        $file = file_get_contents($path);
+                        $base64 = base64_encode($file);
+                        $worker->image = $base64;
+                    }
+                }
+            }
+            return response()->json([
+                'workers' => $workers,
+            ], 200);
+        }catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
     //filter by region
     public function filterByRegion($id)
     {
-        $worker = worker::select('name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
-            ->where('city_id', $id)
-            ->get();
-        return response()->json($worker);
+        try{
+            $workers = Worker::select('name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
+                ->where('city_id', $id)
+                ->get();
+            foreach ($workers as $worker) {
+                if ($worker->image != null) {
+
+                    $path = public_path($worker->image);
+                    if (file_exists($path)) {
+                        $file = file_get_contents($path);
+                        $base64 = base64_encode($file);
+                        $worker->image = $base64;
+                    }
+                }
+            }
+            return response()->json([
+                'workers' => $workers,
+            ], 200);
+        }catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
     //filter by category and region
     public function filterByCategoryAndRegion(Request $request)
     {
-        $category_id = $request->category_id;
-        $city_id = $request->city_id;
-        $worker = worker::select('worker.id','name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
-            ->where('category_id', $category_id)
-            ->where('city_id', $city_id)
-            ->get();
-        return response()->json($worker);
+        try{
+            $category_id = $request->category_id;
+            $city_id = $request->city_id;
+            $workers = Worker::select('worker.id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
+                ->where('category_id', $category_id)
+                ->where('city_id', $city_id)
+                ->get();
+            foreach ($workers as $worker) {
+                if ($worker->image != null) {
+
+                    $path = public_path($worker->image);
+                    if (file_exists($path)) {
+                        $file = file_get_contents($path);
+                        $base64 = base64_encode($file);
+                        $worker->image = $base64;
+                    }
+                }
+            }
+            return response()->json($workers);
+        }catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
 
     //filter by price rate and quality rate and time rate which all integers
     public function filterByPriceRateAndQualityRateAndTimeRate(Request $request)
     {
-        $price_rate = $request->price_rate;
-        $quality_rate = $request->quality_rate;
-        $time_rate = $request->time_rate;
-        $worker = worker::select('worker.id','name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
-            ->join('rate', 'worker.id', '=', 'rate.worker_id')
-            ->where('price_rate', $price_rate)
-            ->where('quality_rate', $quality_rate)
-            ->where('time_rate', $time_rate)
-            ->get();
-        return response()->json($worker);
+        try {
+            $price_rate = $request->price_rate;
+            $quality_rate = $request->quality_rate;
+            $time_rate = $request->time_rate;
+            $workers = Worker::select('worker.id','name', 'phone', 'address', 'city_id', 'image', 'description', 'status')
+                ->join('rate', 'worker.id', '=', 'rate.worker_id')
+                ->where('price_rate', $price_rate)
+                ->where('quality_rate', $quality_rate)
+                ->where('time_rate', $time_rate)
+                ->get();
+            foreach ($workers as $worker) {
+                if ($worker->image != null) {
+
+                    $path = public_path($worker->image);
+                    if (file_exists($path)) {
+                        $file = file_get_contents($path);
+                        $base64 = base64_encode($file);
+                        $worker->image = $base64;
+                    }
+                }
+            }
+            return response()->json([
+                'workers' => $workers,
+            ],200);
+        }catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
 
     //filter by data come from user which may contain category_id and city_id and price_rate and quality_rate and time_rate
     public function filter(Request $request){
         try{
-            $query = worker::select('worker.id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status');
+            $query = Worker::select('worker.id', 'name', 'phone', 'address', 'city_id', 'image', 'description', 'status');
             // check image
 
             if ($request->has('category_id')) {
